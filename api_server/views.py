98c7fdc04be763d0ef55eb6195af211becdada00
HideTake761 <55819922+HideTake761@users.noreply.github.com>
2025-10-28 02:02:55 +0900

    Delete api_server directory

--- api_server/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def health_check(request):
-    """
-    ALBからのヘルスチェックに応答するためのシンプルなビュー
-    常にHTTP 200 OKを返す
-    """
-    return HttpResponse("OK", status=200)
